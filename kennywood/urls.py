ba08f58e5bc499ef8f3904836bfd8a2ae2cc7803
kwohl <ktwohl@gmail.com>
2020-05-20 11:59:04 -0500

    initial commit

---
+++ kennywood/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from kennywoodapi.models import *
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
